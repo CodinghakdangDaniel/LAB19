5be5f788444933b2091269dd39c25c3ba73aee2a
CodinghakdangDaniel <junh2e97@gmail.com>
2023-10-02 13:24:00 +0900

    lab19

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 let apiKey = '0_fThK5YlBIA5fAnnRgQrwquH5KQxSXHnNS3ovneczA';
-const imgBox = document.getElementsByClassName('img-box');
-let inputValue = document.getElementsByClassName('input');
+let inputValue = document.getElementsByClassName('input')[0];
+let page = 2;
+const imgBox = document.getElementById('img-box');
 const btn = document.getElementById('btn');
-console.log(apiKey);
+const showMoreBtn = document.getElementById('show-more-btn');
 
 async function getPhotos() {
     try {
-        const response = await fetch(`https://api.unsplash.com/search/photos?query=${inputValue}&client_id=${apiKey}`);
+        const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${inputValue.value}&client_id=${apiKey}&per_page=12`);
         console.log(response); 
         
-        const data = await response.url
-        console.log(data);
+        const data = await response.json()
+        const results = data.results;
+        
+        results.forEach((result) => {
+            const imgElement = document.createElement('img');
+            imgElement.src = result.urls.small;
+            const imageLink = document.createElement('a');
+            imageLink.href = result.links.html;
+            imageLink.target = '_blank';
 
-        const imgElement = document.createElement('img')
-        imgElement.src = data;
-        document.body.appendChild(imgElement);
+            imageLink.appendChild(imgElement);
+            imgBox.appendChild(imageLink);
+        })
+        showMoreBtn.style.display = 'block'; 
+
     } catch (error) {
         console.error(error)
     }
 };
 
 btn.addEventListener('click', getPhotos);
+
+showMoreBtn.addEventListener('click', () => {
+    page++;
+    getPhotos();
+})
 
